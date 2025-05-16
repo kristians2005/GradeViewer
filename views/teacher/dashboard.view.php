13bd4c6de0a4af26dcf9196d44d6598715daac04
kristians2005 <mikelsons3333@gmail.com>
2025-05-16 08:44:26 +0300

    .

--- views/teacher/dashboard.view.php
+++ views/teacher/dashboard.view.php
@@ -85,27 +85,7 @@
                 Classes Overview
             </h2>
 
-            <!-- Filter Section -->
-            <div class="flex flex-col md:flex-row gap-4 mb-6" data-aos="fade-up" data-aos-delay="250">
-                <div class="form-control flex-1">
-                    <div class="input-group">
-                        <input type="text" id="classSearch" placeholder="Search classes..." class="input input-bordered w-full" />
-                        <button class="btn btn-square">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-                            </svg>
-                        </button>
-                    </div>
-                </div>
-                <div class="form-control">
-                    <select id="sortClasses" class="select select-bordered">
-                        <option value="name-asc">Name (A-Z)</option>
-                        <option value="name-desc">Name (Z-A)</option>
-                        <option value="students-asc">Students (Low to High)</option>
-                        <option value="students-desc">Students (High to Low)</option>
-                    </select>
-                </div>
-            </div>
+        
 
             <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
                 <?php foreach ($classes as $class): ?>
